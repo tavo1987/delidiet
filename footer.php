237d335fde6353d2d30004cceb192eaaa49990ae
tavo <tavo198718@gmail.com>
2017-02-28 21:20:49 -0500

    Updated footer

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 
         </div><!--END CONTAINER-->
 
-        <footer id="footer" role="contentinfo">
-            <div id="copyright">
+        <footer class="footer" role="contentinfo">
+            <div class="copyright">
                 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'shiftpress' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'shiftpress' ), '<a href="">Edwin Ramírez</a>' ); ?>
             </div>
         </footer><!--END FOOTER-->
-    </div><!--END WRAPER-->
+
     <?php wp_footer(); ?>
 </body>
 </html>
