19c7fefe7bae6cb07685710a2505cc4b6b4c5d8e
tavo <tavo198718@gmail.com>
2017-02-28 21:23:10 -0500

    Added js for mobile menu

--- resources/assets/js/shiftpress.js
+++ resources/assets/js/shiftpress.js
@@ -3,5 +3,20 @@
 jQuery(document).foundation();
 
 jQuery(document).ready(function ($) {
-   //Your code here
+    var buttonMenu    = $('#toggle-menu');
+    var menu          = $('.navegation');
+    var buttonSubmenu = $('.menu-item-has-children a');
+    var submenu       = $('.sub-menu');
+
+    buttonMenu.on('click',function(event) {
+        $(this).toggleClass('menu-opened');
+        $(this).next(menu).toggleClass('active-menu');
+    });//END TOGGLE MENU
+
+    buttonSubmenu.on('click', function(event) {
+        event.preventDefault();
+        $(this).toggleClass('active');
+        $(this).next(submenu).stop().slideToggle();
+    });//END BUTTONS SUBMENU
+
 });
