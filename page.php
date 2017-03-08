621b9b8ce3f1a1aff46cd36d75af39fe8e05299c
tavo <tavo198718@gmail.com>
2017-03-08 06:28:46 -0500

    Updated page template

--- page.php
+++ page.php
@@ -1,18 +1,15 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+<section class="sp-content sp-page" role="main">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="header">
-                <h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-            </header>
-            <section class="entry-content">
+            <header class="sp-page__header">
                 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
+                <h1 class="sp-page__title"><?php the_title(); ?></h1>
+            </header>
+            <section class="sp-page__content">
                 <?php the_content(); ?>
-                <div class="entry-links"><?php wp_link_pages(); ?></div>
             </section>
         </article>
-        <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
     <?php endwhile; endif; ?>
 </section>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
