b3d2dac40644d5aeb54cef4a765d344be1b69138
tavo <tavo198718@gmail.com>
2017-03-08 06:29:10 -0500

    Added js for main menu

--- resources/assets/js/shiftpress.js
+++ resources/assets/js/shiftpress.js
@@ -3,20 +3,47 @@
 jQuery(document).foundation();
 
 jQuery(document).ready(function ($) {
-    var buttonMenu    = $('#toggle-menu');
-    var menu          = $('.navegation');
-    var buttonSubmenu = $('.menu-item-has-children a');
-    var submenu       = $('.sub-menu');
 
-    buttonMenu.on('click',function(event) {
-        $(this).toggleClass('menu-opened');
-        $(this).next(menu).toggleClass('active-menu');
-    });//END TOGGLE MENU
+    function offMainMenu() {
+        var buttonMenu    = $('#toggle-menu');
+        var buttonSubmenu = $('.menu-item-has-children a');
 
-    buttonSubmenu.on('click', function(event) {
-        event.preventDefault();
-        $(this).toggleClass('active');
-        $(this).next(submenu).stop().slideToggle();
-    });//END BUTTONS SUBMENU
+        buttonMenu.off();
+        buttonSubmenu.off();
+    }
 
+    function mainMenu() {
+        var buttonMenu    = $('#toggle-menu');
+        var menu          = $('.navegation');
+        var buttonSubmenu = $('.menu-item-has-children a');
+        var submenu       = $('.sub-menu');
+
+        buttonMenu.on('click',function(event) {
+            $(this).toggleClass('menu-opened');
+            $(this).next(menu).toggleClass('active-menu');
+        });//END TOGGLE MENU
+
+        buttonSubmenu.on('click', function(event) {
+            event.preventDefault();
+            $(this).toggleClass('active');
+            $(this).next(submenu).stop().slideToggle();
+        });//END BUTTONS SUBMENU
+    }
+
+    if ($(window).width() < 1024) {
+        mainMenu();
+    }
+
+    $(window).resize(function () {
+        console.log($(window).width());
+        if ($(window).width() >= 1024) {
+            $("nav > ul, nav > ul  li  ul").removeAttr("style");
+            offMainMenu();
+        }
+
+        if ($(window).width() < 1024) {
+            //$("nav > ul, nav > ul  li  ul").removeAttr("style");
+            mainMenu();
+        }
+    });//END RESIZE
 });
