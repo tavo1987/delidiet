a678e1fb4369d5f6f9d81a77e9f0ab70da8cd0e1
tavo <tavo198718@gmail.com>
2017-03-09 06:10:11 -0500

    Fixed js in main menu

--- resources/assets/js/shiftpress.js
+++ resources/assets/js/shiftpress.js
@@ -9,15 +9,18 @@ jQuery(document).ready(function ($) {
     var menu          = $('#main-navigation');
     var submenu       = $('#main-navigation .sub-menu');
 
+    buttonMenu.on('click',function(event) {
+        $(this).toggleClass('menu-opened');
+        $(this).next(menu).toggleClass('active-menu');
+    });//END TOGGLE MENU
+
+
     function offMainMenu() {
-        buttonMenu.off();
         buttonSubmenu.off();
     }
 
-    function mainMenu() {
-    }
-
     function resetMenu() {
+        $("nav > ul, nav > ul  li  ul").removeAttr("style");
         buttonMenu.removeClass('menu-opened');
         buttonSubmenu.removeClass('active');
         submenu.removeAttr('style');
@@ -26,26 +29,19 @@ jQuery(document).ready(function ($) {
 
 
     $(window).on('load resize', function(event) {
-
-        if ($(window).width() > 1000) {
+        resetMenu();
+        if (window.matchMedia('(min-width: 64em)').matches) {
             $("nav > ul, nav > ul  li  ul").removeAttr("style");
             offMainMenu();
             console.log('desktop');
         }else {
-
-            resetMenu();
-            buttonMenu.on('click',function(event) {
-                $(this).toggleClass('menu-opened');
-                $(this).next(menu).toggleClass('active-menu');
-            });//END TOGGLE MENU
-
             buttonSubmenu.on('click', function(event) {
                 event.preventDefault();
                 $(this).toggleClass('active');
                 $(this).next(submenu).stop().slideToggle();
                 event.stopPropagation();
             });//END BUTTONS SUBMENU
-            console.log('mobile');
+            console.log('mobile')
         }
     });
 
