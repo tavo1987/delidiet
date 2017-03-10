34a04084ad7017e8f62a77fdceefc4279e76db35
tavo1987 <tavo198718@gmail.com>
2017-03-09 20:54:58 -0500

    Added footer widget area

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
+    </div><!--END CONTAINER-->
 
-        </div><!--END CONTAINER-->
+    <footer class="footer" role="contentinfo">
 
-        <footer class="footer" role="contentinfo">
-            <div class="copyright">
-                <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'shiftpress' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'shiftpress' ), '<a href="">Edwin Ramírez</a>' ); ?>
-            </div>
-        </footer><!--END FOOTER-->
+        <?php get_sidebar('footer'); ?>
 
+        <div class="copyright">
+            <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'shiftpress' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( '&#174;Powered by: %1$s.', 'shiftpress' ), '<a href="">Gestióon</a>' ); ?>
+        </div>
+    </footer><!--END FOOTER-->
     <?php wp_footer(); ?>
 </body>
 </html>
