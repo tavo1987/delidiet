dc59c3f3e106b99073e9689c349a5892e9ef56d6
tavo <tavo198718@gmail.com>
2017-03-12 16:59:29 -0500

    Updated copyright

--- footer.php
+++ footer.php
@@ -1,11 +1,14 @@
     </div><!--END CONTAINER-->
 
     <footer class="footer" role="contentinfo">
+        <div class="row">
+            <div class="columns">
+                <?php get_sidebar('footer'); ?>
 
-        <?php get_sidebar('footer'); ?>
-
-        <div class="copyright">
-            <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'shiftpress' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( '&#174;Powered by: %1$s.', 'shiftpress' ), '<a href="">Gestióon</a>' ); ?>
+                <div class="copyright">
+                    <?php echo sprintf( __( '&#174;Powered by %1$s.', 'shiftpress' ), '<a href="">Gestión</a>' ); ?>
+                </div>
+            </div>
         </div>
     </footer><!--END FOOTER-->
     <?php wp_footer(); ?>
