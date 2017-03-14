63733459fbecd98fed2fa919660e49ca0609773e
tavo <tavo198718@gmail.com>
2017-03-14 06:15:18 -0500

    Updated front page

--- front-page.php
+++ front-page.php
@@ -5,18 +5,26 @@
     <?php echo do_shortcode('[smartslider3 slider=1]');?>
 </section>
 
-<div class="row">
-    <section id="content" role="main" class="small-12 medium-12 large-9 columns">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'partials/entry' ); ?>
-            <?php comments_template(); ?>
-        <?php endwhile;?>
-            <!-- post navigation -->
-            <div class="pagination">
-                <?php if (function_exists('wp_corenavi')) wp_corenavi(); ?>
-            </div>
-        <?php endif; ?>
-    </section>
-</div>
+<section role="main" class="small-12 medium-12 large-9 columns" class="content">
+    <div class="one-section">
+        <div class="scarsdale-diet">
+            <h2>Dieta</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis rem laudantium, debitis eum sapiente mollitia beatae cumque,
+                quam doloribus, cupiditate ut aliquam at sunt! Similique eius earum magnam iste officiis?
+            </p>
+            <a href="#" class="button button--plus">
+                <i class="icon-plus"></i>
+                SABER MAS
+            </a>
+            <img src="http://delidiet.dev/wp-content/uploads/vegetables.png" alt="Logo">
+        </div>
+
+        <div class="vegetables">
+            <img src="" alt="">
+        </div>
+    </div><!-- . /END ONE SECTION-->
+</section><!-- . /END MAIN-->
+
 <?php get_footer(); ?>
 
