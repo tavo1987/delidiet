55c71e5288001444b3bce287d45f497997c99a6d
tavo <tavo198718@gmail.com>
2017-03-15 06:14:49 -0500

    Added sections to front page

--- front-page.php
+++ front-page.php
@@ -5,32 +5,118 @@
     <?php echo do_shortcode('[smartslider3 slider=1]');?>
 </section>
 
-<section role="main" class="content">
-<div class="row  expanded">
-    <div class="small-12 large-8 columns">
-        <div class="row">
-            <div class="small-12 large-6 columns">
-                <h2>Dieta</h2>
-                <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis rem laudantium, debitis eum sapiente mollitia beatae cumque,
-                    quam doloribus, cupiditate ut aliquam at sunt! Similique eius earum magnam iste officiis?
-                </p>
-                <a href="#" class="button button--plus button--plus--gray">
-                    <i class="icon-plus"></i>
-                    SABER MAS
-                </a>
-            </div><!-- . /END COLUMN-->
+<section role="main" class="content front-page">
+    <!--=====================================================
+            SECTION ONE
+        =====================================================-->
+    <div class="row">
+        <div class="small-12 large-8 columns">
+            <div class="row">
+                <div class="small-12 medium-6 large-6 columns">
+                    <div class="one-section section">
+                        <div class="section__header">
+                            <h2 class="section__title section__title--big">DIETA</h2>
+                            <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
+                        </div><!-- . /END HEADER-->
+                        <p class="section__description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis rem laudantium, debitis eum sapiente mollitia beatae cumque,
+                            quam doloribus, cupiditate ut aliquam at sunt! Similique eius earum magnam iste officiis?
+                        </p>
+                        <a href="#" class="button button--plus button--plus--gray">
+                            <i class="icon-plus"></i>
+                            SABER MAS
+                        </a>
+                    </div><!-- . /END SECTION-->
+                </div><!-- . /END COLUMN-->
 
-            <div class="small-12 large-6 columns">
-                <img src="http://delidiet.dev/wp-content/uploads/vegetables.jpg" alt="Logo">
-            </div><!-- . /END COLUMN-->
-        </div><!-- . /END ROW-->
-    </div>
-    <div class="small-12 large-4 columns">
+                <div class="small-12 medium-6 large-6 text-center columns section">
+                    <img src="http://delidiet.dev/wp-content/uploads/vegetables.png" alt="Logo">
+                </div><!-- . /END COLUMN-->
 
-    </div>
-</div><!-- . /END ROW-->
+                <div class="small-12 columns section"
+                    style="background: url('http://delidiet.dev/wp-content/uploads/background-imc-section.jpg');
+                    background-size: cover;"
+                    >
+                    <div class="section__header">
+                        <h2 class="section__title section__title--white">Calcula tu (IMC) <br>  <strong>Índice de <br> Masa Corporal</strong></h2>
+                    </div><!-- . /END HEADER-->
+                    <p class="section__description section__description--white">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. <br>  Cum sociis natoque penatibus et montes, nascetur ridiculus mus.
+                    </p>
+                </div><!-- . /END COLUMN-->
+            </div><!-- . /END ROW-->
+        </div><!-- . /END COLUMNS-->
+
+        <div class="small-12 large-4 columns section section--white section--align-bottom"
+            style="background: url('http://delidiet.dev/wp-content/uploads/background-almuerzo-light.jpg');
+            background-size: cover;"
+            >
+            <div class="section__header">
+                <img src="http://delidiet.dev/wp-content/uploads/almuerzo.png" alt="Logo">
+                <h2 class="section__title section__title--white  section__title--big" >LIGHT</h2>
+            </div>
+
+            <p class="section__description section__description--white">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
+            </p>
+            <a class="button button--plus" href="#">
+                 <i class="icon-plus"></i>
+                Saber más
+            </a>
+        </div><!-- . /END COLUMN-->
+    </div><!-- . /END ROW-->
+
+    <!--=====================================================
+            SECTION TWO
+        =====================================================-->
+    <div class="row">
+        <div class="small-12 large-8 columns">
+            <div class="row">
+                <div class="small-12 columns section"
+                style="background: url('http://delidiet.dev/wp-content/uploads/background-frutas.jpg') no-repeat right;
+                background-size: contain;"
+                >
+                   <div class="section__header">
+                        <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
+                        <h2 class="section__title section__title--big">DIETA de <br> <strong>14 días</strong></h2>
+                    </div><!-- . /END HEADER-->
+                    <div class="section__subtitle">
+                        <h3 class="gray-color">
+                            PROGRAMA PARA <br>
+                            ADELGAZAR DE UNA <br>MANERA SANA Y RÁPIDA
+                        </h3>
+                    </div><!-- . /END SUBTITLE SECTION4-->
+                    <p class="section__description">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
+                    </p>
+                    <a href="#" class="button button--plus button--plus--gray">
+                       <i class="icon-plus"></i>
+                       SABER MAS
+                   </a>
+                </div><!-- . /END COLUMN-->
+            </div><!-- . /END ROW-->
+        </div><!-- . /END COLUMN-->
+
+        <div class="small-12 large-4 columns">
+            <div class="row">
+                <div class="small-12 columns section">
+                    <h2 class="section__title gray-color"><strong>SIN</strogn> <br>PASTILLAS</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum sunt voluptatem soluta obcaecati ex hic, maxime quod voluptates similique iusto nulla. Nesciunt porro maxime nulla dolores, ducimus tenetur, ex molestias.
+                    </p>
+                </div>
+                <div class="small-12 columns section">
+                    <div class="section__header">
+                        <h2 class="section__title"><strong class="gray-color">SERVICIO A</strong><br">DOMICILIO</h2>
+                    </div>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum sunt voluptatem soluta obcaecati ex hic, maxime quod voluptates similique iusto nulla. Nesciunt porro maxime nulla dolores, ducimus tenetur, ex molestias.
+                    </p>
+                </div><!-- . /END COLUMN-->
+            </div><!-- . /END ROW-->
+        </div><!-- . /END COLUMN-->
+    </div><!-- . /END ROW-->
 
 </section><!-- . /END MAIN-->
 
