8b460b140d5a7f3ae08c489a1afbe862840cf8b7
tavo1987 <tavo198718@gmail.com>
2017-03-14 21:02:48 -0500

    Updated front page

--- front-page.php
+++ front-page.php
@@ -5,25 +5,33 @@
     <?php echo do_shortcode('[smartslider3 slider=1]');?>
 </section>
 
-<section role="main" class="small-12 medium-12 large-9 columns" class="content">
-    <div class="one-section">
-        <div class="scarsdale-diet">
-            <h2>Dieta</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis rem laudantium, debitis eum sapiente mollitia beatae cumque,
-                quam doloribus, cupiditate ut aliquam at sunt! Similique eius earum magnam iste officiis?
-            </p>
-            <a href="#" class="button button--plus">
-                <i class="icon-plus"></i>
-                SABER MAS
-            </a>
-            <img src="http://delidiet.dev/wp-content/uploads/vegetables.png" alt="Logo">
-        </div>
+<section role="main" class="content">
+<div class="row  expanded">
+    <div class="small-12 large-8 columns">
+        <div class="row">
+            <div class="small-12 large-6 columns">
+                <h2>Dieta</h2>
+                <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis rem laudantium, debitis eum sapiente mollitia beatae cumque,
+                    quam doloribus, cupiditate ut aliquam at sunt! Similique eius earum magnam iste officiis?
+                </p>
+                <a href="#" class="button button--plus button--plus--gray">
+                    <i class="icon-plus"></i>
+                    SABER MAS
+                </a>
+            </div><!-- . /END COLUMN-->
 
-        <div class="vegetables">
-            <img src="" alt="">
-        </div>
-    </div><!-- . /END ONE SECTION-->
+            <div class="small-12 large-6 columns">
+                <img src="http://delidiet.dev/wp-content/uploads/vegetables.jpg" alt="Logo">
+            </div><!-- . /END COLUMN-->
+        </div><!-- . /END ROW-->
+    </div>
+    <div class="small-12 large-4 columns">
+
+    </div>
+</div><!-- . /END ROW-->
+
 </section><!-- . /END MAIN-->
 
 <?php get_footer(); ?>
