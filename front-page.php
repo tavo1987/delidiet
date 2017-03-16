1842e97f97d2805b1e64f5c41627a0835caac58b
tavo <tavo198718@gmail.com>
2017-03-16 08:39:54 -0500

    Added featured section and footer banner

--- front-page.php
+++ front-page.php
@@ -72,30 +72,28 @@
     <div class="section-two bg-color">
         <div class="row">
             <div class="small-12 large-8 columns background-section background-section--hide-small" data-background-src="http://delidiet.dev/wp-content/uploads/background-fruits.png">
-
-                    <div class=" section">
-                       <div class="section__header">
-                            <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
-                            <h2 class="section__title section__title--big">DIETA de <br> <strong>14 días</strong></h2>
-                        </div><!-- . /END HEADER-->
-                        <div class="section__subtitle">
-                            <h3 class="gray-color">
-                                PROGRAMA PARA <br>
-                                ADELGAZAR DE UNA <br>MANERA SANA Y RÁPIDA
-                            </h3>
-                        </div><!-- . /END SUBTITLE SECTION4-->
-                        <p class="section__description">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
-                        </p>
-                        <a href="#" class="button button--plus button--plus--gray">
-                           <i class="icon-plus"></i>
-                           SABER MAS
-                       </a>
-                    </div><!-- . /END SECTION-->
-
+                <div class=" section">
+                   <div class="section__header">
+                        <img src="http://delidiet.dev/wp-content/uploads/logo-scarsdale-1.png" alt="Test">
+                        <h2 class="section__title section__title--big">DIETA de <br> <strong>14 días</strong></h2>
+                    </div><!-- . /END HEADER-->
+                    <div class="section__subtitle">
+                        <h3 class="gray-color">
+                            PROGRAMA PARA <br>
+                            ADELGAZAR DE UNA <br>MANERA SANA Y RÁPIDA
+                        </h3>
+                    </div><!-- . /END SUBTITLE SECTION4-->
+                    <p class="section__description">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
+                    </p>
+                    <a href="#" class="button button--plus button--plus--gray">
+                       <i class="icon-plus"></i>
+                       SABER MAS
+                   </a>
+                </div><!-- . /END SECTION-->
             </div><!-- . /END COLUMN-->
 
-            <div class="small-12 large-4 columns">
+            <div class="small-12 large-4 columns hide-for-small-only">
                 <div class="row">
                     <div class="small-12 columns section section--padding-left">
                         <h2 class="section__title section__title--gray section__title--padding-top-large">
@@ -121,9 +119,75 @@
                 </div><!-- . /END ROW-->
             </div><!-- . /END COLUMN-->
         </div><!-- . /END ROW-->
-    </div><!--./ END SECTION CONTAINER-->
+    </div><!--./ END SECTION TWO-->
 
-</section><!-- . /END MAIN-->
+    <!--=====================================================
+            FEATURED SECTION
+        =====================================================-->
+    <div class="featured-section bg-color">
+        <div class="featured-section__header">
+            <div class="row">
+                <div class="columns">
+                    <img class="featured-section__image" src="http://delidiet.dev/wp-content/uploads/girl-recomendation.png" alt="Feaured section image">
+                </div><!--./ COLUMNS-->
+            </div><!--./ FEAURED ROW-->
+        </div><!--./ FEAURED IMAGE HEADER-->
+
+        <div class="row featured-section__content">
+            <div class="small-12 medium-12 large-4 columns section section--padding-top-none small-text-center">
+                <div class="row aling-center">
+                    <div class="small-12 medium-6 large-12 columns">
+                        <div class="section__header">
+                            <h2 class="section__title  section__title--green section__title--padding-botom, section__title--padding-top" >
+                                ¿Cómo <br>
+                                <strong>funciona?</strong>
+                            </h2>
+                            <img src="http://delidiet.dev/wp-content/uploads/black-scarsdale-logo.png" alt="Logo">
+                        </div>
+                    </div><!-- . /END COLUMN-->
+
+                    <div class="small-12 medium-6 large-12 columns  padding-top--medium">
+                        <p class="section__description section__description--padding-bottom-none">
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
+                        </p>
+                        <a class="button button--plus button--plus--gray" href="#">
+                             <i class="icon-plus"></i>
+                            Saber más
+                        </a>
+                    </div><!-- . /END COLUMN-->
+                </div><!--./ END ROW-->
+            </div><!-- . /END COLUMN-->
+
+            <div class="small-12 medium-12 large-8 columns">
+                <div class="responsive-embed widescreen">
+                    <iframe width="640" height="360" src="https://www.youtube-nocookie.com/embed/LlzFfE2gvcw?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+            </div><!-- . /END COLUMN-->
+        </div><!-- . /END ROW-->
+    </div><!-- . /END FEATURED SECTION-->
+
+    <!--=====================================================
+            FOOTER BANNER
+        =====================================================-->
+    <div class="background-section parallax" data-background-src="http://delidiet.dev/wp-content/uploads/banner.jpg">
+        <div class="row align-center">
+            <div class="small-12 medium-8 large-7 columns background-section">
+                <div class=" section">
+                   <div class="section__header">
+                        <h2 class="section__title section__title--white">¡TE <br> <strong>VISITAMOS!</strong></h2>
+                    </div><!-- . /END HEADER-->
+                    <p class="section__description section__description--full-width section__description--white">
+                        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis.
+                    </p>
+                    <a href="#" class="button button--plus">
+                       <i class="icon-plus"></i>
+                       SABER MAS
+                   </a>
+                </div><!-- . /END SECTION-->
+            </div><!-- . /END COLUMN-->
+        </div><!-- . /END ROW-->
+    </div><!-- ./ BACKGROUND SECTION-->
+</section><!-- ./END MAIN-->
 
 <?php get_footer(); ?>
 
